1276eb0352d7267e41dfa5ff88b1649eac2d9f24
Gleb <dyatlov.gleb@gmail.com>
2021-12-18 20:50:51 +0300

    test

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ function handleSSE(res, connections = []) {
 function sendSSE(data, connections = []) {
     connections.forEach(connection => {
         const id = new Date().toISOString()
-        connection.write('retry:' + 1 + '\n')
+        connection.write('retry:' + 5000 + '\n')
         connection.write('data: ' + JSON.stringify(data) + '\n')
         connection.write('id: ' + id + '\n\n')
     })
